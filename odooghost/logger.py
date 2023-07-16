d4896e58e5274004c6eb0fd133a5d9b16343a73a
Remy Zulauff <remy.zulauff@gmail.com>
2023-07-16 16:09:35 +0200

    feat(logger.py): add InterceptHandler class to convert default logging LogRecord to loguru format feat(logger.py): add setup_cli_logging function to configure logger for CLI feat(pyproject.toml): add loguru dependency to enable loguru logging library in the project fix(pyproject.toml): fix odooghost script entry point to point to the correct module and function

---
+++ odooghost/logger.py
@@ -0,0 +1,44 @@
+import logging
+import sys
+
+from loguru import logger
+
+
+class InterceptHandler(logging.Handler):
+    """
+    InterceptHandler convert default logging LogRecord to loguru format
+    """
+
+    def emit(self, record: logging.LogRecord):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+def setup_cli_logging() -> None:
+    """
+    Setup logger for CLI
+    """
+    logger.configure(
+        handlers=[
+            dict(
+                sink=sys.stderr,
+                backtrace=True,
+                diagnose=True,
+                level="DEBUG",
+                format="<level>{message}</level>",
+            )
+        ]
+    )
