32c14d53189625968a4ea4f2815e23ea6db0ea58
Remy Zulauff <remy.zulauff@gmail.com>
2023-07-16 17:38:44 +0200

    feat(config.py): add version and working_dir commands to the CLI for printing config version and working directory respectively fix(config.py): remove NotImplementedError from version and working_dir commands as they are not yet implemented

--- odooghost/cli/config.py
+++ odooghost/cli/config.py
@@ -3,6 +3,22 @@ import typer
 cli = typer.Typer(no_args_is_help=True)
 
 
+@cli.command()
+def version() -> None:
+    """
+    Print config version
+    """
+    raise NotImplementedError()
+
+
+@cli.command()
+def working_dir() -> None:
+    """
+    Print working directory
+    """
+    raise NotImplementedError()
+
+
 @cli.callback()
 def callback() -> None:
     """
