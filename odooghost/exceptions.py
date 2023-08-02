68661af8612e9b2bec07f4d5a3e05ac5f1fe7576
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-02 23:53:35 +0200

    feat(exceptions.py): add new exception classes StackException, StackImageEnsureError, and StackImagePullError to handle specific errors related to stack operations

--- odooghost/exceptions.py
+++ odooghost/exceptions.py
@@ -4,3 +4,15 @@ class OdooGhostException(Exception):
 
 class ContextAlreadySetupError(OdooGhostException):
     ...
+
+
+class StackException(OdooGhostException):
+    ...
+
+
+class StackImageEnsureError(StackException):
+    ...
+
+
+class StackImagePullError(StackImageEnsureError):
+    ...
