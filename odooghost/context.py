135d0ae67f09cde4d089b71aa8df13f15a1f4c8f
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-02 23:51:55 +0200

    refactor(context.py): add docker client property to Context class for better code organization and reusability feat(context.py): add docker import statement to support docker client usage in Context class

--- odooghost/context.py
+++ odooghost/context.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import docker
 import yaml
 
 from odooghost import constant, exceptions
@@ -11,6 +12,7 @@ class Context:
         self._config_path = self._app_dir / "config.yml"
         self._data_dir = self._app_dir / "data"
         self._plugins_dir = self._app_dir / "plugins"
+        self._docker_client = None
 
     def check_setup_state(self) -> bool:
         return self._app_dir.exists()
@@ -27,6 +29,12 @@ class Context:
         )
         with open(self._config_path.as_posix(), "w") as stream:
             yaml.dump(config_data, stream=stream)
+
+    @property
+    def docker(self) -> docker.Client:
+        if not self._docker_client:
+            self._docker_client = docker.from_env()
+        return self._docker_client
 
 
 ctx = Context()
