575699037d35d94b7d45c66322a24ce162d63eaa
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-05 15:27:39 +0200

    fix(odoo.py): add missing import statements for sys, BytesIO, logger, exceptions, renderer, utils, and ctx feat(odoo.py): add support for building custom Odoo image with specified dependencies feat(odoo.py): add property to generate base image tag based on Odoo version feat(odoo.py): add property to generate image tag for custom Odoo image based on stack name and Odoo version feat(odoo.py): add property to check if custom image is available for Odoo service

--- odooghost/services/odoo.py
+++ odooghost/services/odoo.py
@@ -1,4 +1,11 @@
+import sys
 import typing as t
+from io import BytesIO
+
+from loguru import logger
+
+from odooghost import exceptions, renderer, utils
+from odooghost.context import ctx
 
 from .base import BaseService
 
@@ -7,16 +14,43 @@ if t.TYPE_CHECKING:
 
 
 class OdooService(BaseService):
-    def __init__(self, config: "config.OdooStackConfig") -> None:
+    def __init__(self, stack_name: str, config: "config.OdooStackConfig") -> None:
+        self.stack_name = stack_name
         self._config = config
         super().__init__()
 
     def build_image(self) -> None:
         super().build_image()
+        logger.debug("Building Odoo custom image")
+        try:
+            all_events = list(
+                utils.progress_stream.stream_output(
+                    ctx.docker.api.build(
+                        fileobj=BytesIO(
+                            renderer.render_dockerfile(
+                                odoo_version=self._config.version,
+                                apt_dependencies=self._config.dependencies.apt,
+                                pip_dependencies=self._config.dependencies.python,
+                            ).encode("utf-8")
+                        ),
+                        tag=self.image_tag,
+                    ),
+                    sys.stdout,
+                )
+            )
+            print(all_events)
+        except exceptions.StreamOutputError:
+            raise exceptions.StackImageBuildError(
+                "Failed to build Odoo image, check dependencies"
+            )
+
+    @property
+    def base_image_tag(self) -> str:
+        return f"odoo:{self._config.version}"
 
     @property
     def image_tag(self) -> str:
-        return f"odoo:{self._config.version}"
+        return f"odooghost_{self.stack_name}:{self._config.version}".lower()
 
     @property
     def has_custom_image(self) -> bool:
