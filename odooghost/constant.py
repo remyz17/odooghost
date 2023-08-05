42d7cbd046cb7c3a6a4e056d46b2fc4aaf7ceeee
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-05 13:25:01 +0200

    chore(constant.py): add SRC_DIR constant to store the absolute path of the current file's parent directory feat(constant.py): add LABEL_STACKNAME constant to store the label for the stackname in the application

--- odooghost/constant.py
+++ odooghost/constant.py
@@ -5,3 +5,5 @@ import typer
 APP_DIR: pathlib.Path = pathlib.Path(
     typer.get_app_dir(app_name="odooghost", force_posix=True)
 ).resolve()
+SRC_DIR: pathlib.Path = pathlib.Path(__file__).absolute().parent
+LABEL_STACKNAME: str = "odooghost_stackname"
