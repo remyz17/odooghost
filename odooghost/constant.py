fc93ec94c0f81f7ae6444d39ceb85ed8b8b8b44f
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-14 20:23:40 +0200

    chore(constant.py): refactor constant names for improved readability and consistency feat(constant.py): add new constant LABEL_STACK_SERVICE_TYPE to represent the stack type of odooghost

--- odooghost/constant.py
+++ odooghost/constant.py
@@ -6,5 +6,7 @@ APP_DIR: pathlib.Path = pathlib.Path(
     typer.get_app_dir(app_name="odooghost", force_posix=True)
 ).resolve()
 SRC_DIR: pathlib.Path = pathlib.Path(__file__).absolute().parent
-LABEL_STACKNAME: str = "odooghost_stackname"
-COMMON_NETWORK_NAME: str = "odooghost_bridge"
+LABEL_NAME: str = "odooghost"
+LABEL_STACKNAME: str = f"{LABEL_NAME}_stackname"
+LABEL_STACK_SERVICE_TYPE: str = f"{LABEL_NAME}_stack_type"
+COMMON_NETWORK_NAME: str = f"{LABEL_NAME}_bridge"
