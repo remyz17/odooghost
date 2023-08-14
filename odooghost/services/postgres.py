9c32f32cf85490abb4652232b9835570368814c9
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-14 19:00:31 +0200

    refactor(postgres.py): Follow last BaseService changes
    feat(postgres.py):
    - Add support for creating volumes for the db container
    - Add support for creating the db container with the necessary environment variables and mounts

--- odooghost/services/postgres.py
+++ odooghost/services/postgres.py
@@ -1,6 +1,11 @@
 import typing as t
 
+from docker.errors import APIError
+from docker.types import Mount
 from loguru import logger
+
+from odooghost import constant, exceptions
+from odooghost.context import ctx
 
 from .base import BaseService
 
@@ -10,17 +15,56 @@ if t.TYPE_CHECKING:
 
 class PostgresService(BaseService):
     def __init__(self, stack_name: str, config: "config.PostgresStackConfig") -> None:
-        self.stack_name = stack_name
         self._config = config
-        super().__init__()
+        super().__init__(name="db", stack_name=stack_name)
+
+    def _get_container_labels(self) -> dict[str, str]:
+        return super()._get_container_labels()
 
     def ensure_base_image(self, do_pull: bool = False) -> None:
         if self._config.type == "remote":
             logger.debug("Skip postgres image as it's remote type")
         return super().ensure_base_image(do_pull)
 
-    def build_image(self) -> None:
-        return super().build_image()
+    def build_image(self, rm: bool = True, no_cache: bool = False) -> None:
+        return super().build_image(rm=rm, no_cache=no_cache)
+
+    def create_volumes(self) -> None:
+        try:
+            ctx.docker.volumes.create(name=self.volume_name, driver="local")
+        except APIError as err:
+            raise exceptions.StackVolumeCreateError(
+                f"Failed to create db volume: {err}"
+            )
+
+    def create_container(self) -> None:
+        try:
+            ctx.docker.containers.create(
+                name=self.container_name,
+                image=self.base_image_tag,
+                hostname="db",
+                labels=self._get_container_labels(),
+                environment={
+                    "POSTGRES_DB": "postgres",
+                    "POSTGRES_PASSWORD": "odoo",
+                    "POSTGRES_USER": "odoo",
+                },
+                mounts=[
+                    Mount(
+                        source=self.volume_name,
+                        target="/var/lib/postgresql/data",
+                        type="volume",
+                    )
+                ],
+                network=constant.COMMON_NETWORK_NAME,
+            )
+        except APIError as err:
+            raise exceptions.StackContainerCreateError(
+                f"Failed to create db container: {err}"
+            )
+
+    def create(self, do_pull: bool) -> None:
+        return super().create(do_pull=do_pull)
 
     @property
     def is_remote(self) -> bool:
