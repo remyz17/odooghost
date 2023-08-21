b7449a8f3cf336fff14ea0ca645b9802ed5a0a22
Remy Zulauff <remy.zulauff@gmail.com>
2023-08-21 18:30:52 +0200

    refactor(base.py): remove abstractmethod decorator from some methods feat(base.py): add create_container method to create a new container with given options feat(base.py): add start_container method to start the container associated with the service feat(base.py): add get_container method to retrieve the container associated with the service

--- odooghost/services/base.py
+++ odooghost/services/base.py
@@ -1,14 +1,14 @@
 import abc
+import typing as t
 
 from docker.errors import APIError, ImageNotFound, NotFound
 from loguru import logger
 
 from odooghost import constant, exceptions
+from odooghost.container import Container
 from odooghost.context import ctx
-
-
-def labels_as_list(labels: dict[str, str]) -> list[str]:
-    return [f"{k}={v}" for k, v in labels.items()]
+from odooghost.types import Filters, Labels
+from odooghost.utils.docker import labels_as_list
 
 
 class BaseService(abc.ABC):
@@ -16,7 +16,6 @@ class BaseService(abc.ABC):
         self.name = name
         self.stack_name = stack_name
 
-    @abc.abstractmethod
     def _get_container_labels(self) -> dict[str, str]:
         return {
             constant.LABEL_NAME: "true",
@@ -42,12 +41,10 @@ class BaseService(abc.ABC):
                 f"Failed to get image {self.base_image_tag}: {err}"
             )
 
-    @abc.abstractmethod
     def build_image(self, rm: bool = True, no_cache: bool = False) -> None:
         if not self.has_custom_image:
             return None
 
-    @abc.abstractmethod
     def drop_image(self) -> None:
         if self.has_custom_image:
             try:
@@ -57,7 +54,6 @@ class BaseService(abc.ABC):
             except APIError as err:
                 logger.error(f"Failed to drop image {self.image_tag}: {err}")
 
-    @abc.abstractmethod
     def create_volumes(self) -> None:
         try:
             ctx.docker.volumes.create(
@@ -70,7 +66,6 @@ class BaseService(abc.ABC):
                 f"Failed to create {self.name} volume: {err}"
             )
 
-    @abc.abstractmethod
     def drop_volumes(self) -> None:
         try:
             volume = ctx.docker.volumes.get(self.volume_name)
@@ -80,29 +75,71 @@ class BaseService(abc.ABC):
         except APIError as err:
             logger.error(f"Failed to drop volume {volume.id}: {err}")
 
-    @abc.abstractmethod
-    def create_container(self) -> None:
-        ...
+    def containers(
+        self,
+        filters: t.Optional[Filters] = None,
+        labels: t.Optional[Labels] = None,
+        stopped: bool = True,
+    ) -> t.List[Container]:
+        if filters is None:
+            filters = {}
+        filters.update(
+            {
+                "label": labels_as_list(self._get_container_labels())
+                + (labels_as_list(labels) if labels else [])
+            }
+        )
+        return list(
+            filter(
+                None,
+                [
+                    Container.from_ps(container)
+                    for container in ctx.docker.api.containers(
+                        all=stopped, filters=filters
+                    )
+                ],
+            )
+        )
 
     @abc.abstractmethod
+    def create_container(self, **options) -> Container:
+        return Container.create(**options)
+
     def drop_containers(self, all: bool = True, force: bool = True) -> None:
-        for container in ctx.docker.containers.list(
-            all=all,
-            filters={"label": labels_as_list(self._get_container_labels())},
-        ):
+        for container in self.containers(stopped=all):
             try:
                 container.remove(force=force)
             except APIError as err:
                 logger.error(f"Failed to drop container {container.id}: {err}")
 
-    @abc.abstractmethod
+    def get_container(self) -> Container:
+        try:
+            return Container.from_id(id=self.container_name)
+        except NotFound:
+            raise exceptions.StackContainerNotFound(
+                f"Container {self.container_name} not found !"
+            )
+        except APIError as err:
+            raise exceptions.StackContainerGetError(
+                f"Failed to get container {self.container_name} : {err}"
+            )
+
+    def start_container(self) -> Container:
+        container = self.get_container()
+        try:
+            container.start()
+            return container
+        except APIError:
+            raise exceptions.StackContainerStartError(
+                f"Failed to start container {container.id}"
+            )
+
     def create(self, do_pull: bool) -> None:
         self.ensure_base_image(do_pull=do_pull)
         self.build_image()
         self.create_volumes()
         self.create_container()
 
-    @abc.abstractmethod
     def drop(self, volumes: bool = True) -> None:
         self.drop_containers()
         if volumes:
