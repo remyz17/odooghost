3aa16e60f5cba46b79055e6c7431d6938d179bc8
Remy Zulauff <remy.zulauff@gmail.com>
2023-09-14 21:01:33 +0200

    feat(misc.py): add function to generate a hash of a given string using MD5 algorithm for data integrity purposes

--- odooghost/utils/misc.py
+++ odooghost/utils/misc.py
@@ -1,2 +1,9 @@
+import hashlib
+
+
 def labels_as_list(labels: dict[str, str]) -> list[str]:
     return [f"{k}={v}" for k, v in labels.items()]
+
+
+def get_hash(data: str) -> str:
+    return hashlib.md5(data.encode(), usedforsecurity=False).hexdigest()[:8]
