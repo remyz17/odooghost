e4d5cbed0b2e958b0e299345569b3ce3b366b2b3
Remy Zulauff <remy.zulauff@gmail.com>
2023-11-17 19:37:34 +0100

    feat(exec.py): add set_permissions function to set permissions and ownership for a given path inside a Docker container

--- odooghost/utils/exec.py
+++ odooghost/utils/exec.py
@@ -16,6 +16,18 @@ def remove_inode(container: "Container", inode_path: str) -> bool:
     return exit_code == 0
 
 
-def create_folder(container: "Container", folder_path: str) -> int:
+def create_folder(container: "Container", folder_path: str) -> bool:
     exit_code, _ = container.exec_run(command=f"mkdir {folder_path}")
+    return exit_code == 0
+
+
+def set_permissions(
+    container: "Container", path: str, user: str = "odoo", group: str = "odoo"
+):
+    """
+    Set permissions and ownership for a given path inside a Docker container.
+    """
+    exit_code, _ = container.exec_run(
+        command=f"chown -R {user}:{group} {path}", user="root"
+    )
     return exit_code == 0
