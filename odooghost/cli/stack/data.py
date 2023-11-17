2c897a16ead302cfe98b064b30057ffbbb935f9c
Remy Zulauff <remy.zulauff@gmail.com>
2023-11-17 18:17:29 +0100

    feat(data.py): add support for restoring data in Stack

--- odooghost/cli/stack/data.py
+++ odooghost/cli/stack/data.py
@@ -1,4 +1,5 @@
 import typing as t
+from contextlib import nullcontext
 from pathlib import Path
 
 import typer
@@ -64,7 +65,7 @@ def dump(
             container=db_container, dbname=dbname, jobs=jobs, format=pg_format
         )
         if exit_code != 0 and not typer.confirm(
-            "Database restore exited with non 0 code, would you like to continue ?"
+            "pg_dump exited with non 0 code, would you like to continue ?"
         ):
             raise typer.Abort()
 
@@ -73,7 +74,7 @@ def dump(
         data, _ = db_container.get_archive(path=dump_path)
         dest_path = dest / f"{stack.name}_dump_db_{dbname}_{now}.tar"
         misc.write_tar(dest=dest_path, data=data)
-        logger.info(f"Writed dump file at {dest_path.as_posix()}")
+        logger.info(f"Transfered dump file at {dest_path.as_posix()}")
 
         odoo_container = t.cast(
             "Container", stack.get_service(name="odoo").get_container()
@@ -82,15 +83,16 @@ def dump(
         if not exec.folder_exists(container=odoo_container, folder_path=filestore_path):
             logger.warning(f"Filestore at {filestore_path} doest not exists !")
             return
-        logger.info("Dumping filestore ...")
+
         logger.debug("Transfering filestore from container ...")
         data, _ = odoo_container.get_archive(path=filestore_path)
         dest_path = dest / f"{stack.name}_dump_filestore_{dbname}_{now}.tar"
         misc.write_tar(dest=dest_path, data=data)
-        logger.info(f"Writed filestore at {dest_path.as_posix()}")
+        logger.info(f"Transfered filestore at {dest_path.as_posix()}")
         logger.info(f"Done dumping stack {stack_name} data !")
-    except exceptions.StackException:
-        pass
+    except exceptions.StackException as err:
+        logger.error(f"Failed to dump {stack_name} data !")
+        logger.error(err)
 
 
 @cli.command()
@@ -115,24 +117,108 @@ def restore(
         t.Optional[Path],
         typer.Argument(
             file_okay=True,
-            dir_okay=False,
+            dir_okay=True,
             readable=True,
             resolve_path=True,
             exists=True,
-            help="Database dump path",
+            help="Database filestore path",
         ),
     ] = None,
     jobs: t.Annotated[
-        int, typer.Option("-j", "--jobs", help="Postgres pg_dump jobs")
+        int,
+        typer.Option(
+            "-j",
+            "--jobs",
+            help="Postgres pg_restore jobs (Only availible in --pg-format=d)",
+        ),
     ] = 4,
+    force: t.Annotated[
+        bool,
+        typer.Option(
+            "-f",
+            "--force",
+            help="Drop database if already exists",
+        ),
+    ] = False,
 ) -> None:
     """
-    Restore
+    Restore database and/or filestore in Stack
     """
     try:
-        pass
-    except exceptions.StackException:
-        pass
+        stack = Stack.from_name(name=stack_name)
+        db_container = t.cast("Container", stack.get_service(name="db").get_container())
+
+        if db.database_exsits(container=db_container, dbname=dbname):
+            if not force:
+                logger.error(f"Database {dbname} already exists !")
+                raise typer.Abort()
+            if db.drop_database(container=db_container, dbname=dbname) != 0:
+                logger.error(f"Failed to drop database {dbname}")
+                raise typer.Abort()
+
+        logger.info("Transfering dump to container ...")
+        dest_dump_path = Path(
+            f"/tmp/odooghost_restore_{dbname}_{misc.get_now()}"  # nosec B108
+        )
+
+        with nullcontext() if misc.is_tarfile(dump_path) else misc.temp_tar_gz_file(
+            dump_path
+        ) as tar_dump_path:
+            if tar_dump_path is None:
+                tar_dump_path = dump_path
+            exec.create_folder(container=db_container, folder_path=dest_dump_path)
+            with open(tar_dump_path, "rb") as stream:
+                db_container.put_archive(path=dest_dump_path, data=stream)
+
+        logger.info("Creating database ...")
+        if db.create_database(container=db_container, dbname=dbname) != 0:
+            logger.error(f"Failed to create database {dbname}!")
+            raise typer.Abort()
+
+        logger.info("Restoring dump ...")
+        if db.restore_database(
+            container=db_container,
+            dbname=dbname,
+            dump_path=dest_dump_path / dump_path.name,
+            jobs=jobs,
+        ) != 0 and not typer.confirm(
+            "pg_restore exited with non 0 code, would you like to continue ?"
+        ):
+            logger.error("Failed to restore database !")
+            raise typer.Abort()
+
+        if filestore_path:
+            odoo_container = t.cast(
+                "Container", stack.get_service(name="odoo").get_container()
+            )
+            dest_filestore_path = odoo.get_filestore_path(dbname=dbname)
+            if exec.folder_exists(
+                container=odoo_container, folder_path=dest_filestore_path
+            ):
+                if not force:
+                    logger.error("Filestore path already exists !")
+                    raise typer.Abort()
+
+                exec.remove_inode(
+                    container=odoo_container, inode_path=dest_filestore_path
+                )
+
+            logger.info("Transfering filestore to container ...")
+            with nullcontext() if misc.is_tarfile(
+                filestore_path
+            ) else misc.temp_tar_gz_file(filestore_path) as tar_filestore_path:
+                if tar_filestore_path is None:
+                    tar_filestore_path = filestore_path
+                exec.create_folder(
+                    container=odoo_container, folder_path=dest_filestore_path
+                )
+                with open(tar_filestore_path, "rb") as stream:
+                    odoo_container.put_archive(path=dest_filestore_path, data=stream)
+
+        logger.info(f"Done restoring stack {stack_name} data !")
+    except exceptions.StackException as err:
+        logger.error(f"Failed to dump {stack_name} data !")
+        logger.error(err)
 
 
 @cli.callback()
