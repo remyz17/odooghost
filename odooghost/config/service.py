38c6607d28978ad8c1d3f000224cf629a8e27529
Remy Zulauff <remy.zulauff@gmail.com>
2023-11-18 18:27:05 +0100

    refactor(addons): move addons manager directly inside config module

--- odooghost/config/service.py
+++ odooghost/config/service.py
@@ -3,6 +3,8 @@ import typing as t
 
 from pydantic import BaseModel, validator
 
+from odooghost import exceptions
+
 from . import addons as _addons
 from . import dependency
 
@@ -86,6 +88,79 @@ class OdooStackConfig(StackServiceConfig):
         if v not in (11.0, 12.0, 13.0, 14.0, 15.0, 16.0, 17.0):
             raise ValueError(f"Unsuported Odoo version {v}")
         return v
+
+    def _get_addons(
+        self, mode: t.Optional[str] = None
+    ) -> t.Generator[_addons.AddonsConfig, None, None]:
+        for addon_config in self.addons:
+            if mode is None:
+                yield addon_config
+            elif addon_config.mode == mode:
+                yield addon_config
+
+    def get_copy_addons(self) -> t.Generator[_addons.AddonsConfig, None, None]:
+        """
+        Yields addons configurations that are set to copy mode.
+
+        Returns:
+            t.Generator[AddonsConfig, None, None]: Copy addons
+        """
+        yield from self._get_addons(mode="copy")
+
+    def get_mount_addons(self) -> t.Generator[_addons.AddonsConfig, None, None]:
+        """
+        Yields addons configurations that are set to mount mode.
+
+        Returns:
+            t.Generator[AddonsConfig, None, None]: Mount addons
+        """
+        yield from self._get_addons(mode="mount")
+
+    def get_addons_path(self) -> str:
+        """
+        Returns a comma-separated string of all addons paths.
+
+        Returns:
+            str: addons paths
+        """
+        return ",".join(
+            map(lambda addons: addons.container_posix_path, self._get_addons())
+        )
+
+    def ensure_addons(self) -> None:
+        """
+        Validates the addons paths, raising an error for invalid paths.
+
+        Raises:
+            exceptions.InvalidAddonsPathError: When addons path is not valid
+        """
+        for addons in self._get_addons():
+            addons.validate()
+            if not _addons.is_addons_path(addons_path=addons.path):
+                raise exceptions.InvalidAddonsPathError(
+                    f"Addons path {addons.path.as_posix()} is not a valid addons path"
+                )
+
+    @property
+    def has_copy_addons(self) -> bool:
+        """
+        Checks if any addons is set to copy mode.
+
+        Returns:
+            bool:
+        """
+        return any(addon_config.mode == "copy" for addon_config in self.addons)
+
+    @property
+    def has_mount_addons(self) -> bool:
+        """
+        Checks if any addons is set to mount mode.
+
+
+        Returns:
+            bool:
+        """
+        return any(addon_config.mode == "mount" for addon_config in self.addons)
 
 
 class StackServicesConfig(BaseModel):
