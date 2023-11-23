bb9de5f3806466d6a70f5ab26abab22e9c57d1df
Remy Zulauff <remy.zulauff@gmail.com>
2023-11-23 23:18:53 +0100

    feat(addons.py): add support for optional addons type and git origin in AddonsConfig model

--- odooghost/types.py
+++ odooghost/types.py
@@ -1,5 +1,26 @@
 import typing as t
 
+import giturlparse
+
 Filters = t.Dict[str, str]
 Labels = Filters
 Attrs = t.Dict[str, t.Any]
+AddonsType = t.Literal["remote", "local"]
+
+
+class GitOrigin:
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v, _):
+        if not isinstance(v, str):
+            raise TypeError("string required")
+
+        parsed = giturlparse.parse(v)
+        if not parsed.valid:
+            raise ValueError("invalid git url")
+
+        # Retourner l'objet giturlparse plutôt que la chaîne
+        return parsed
