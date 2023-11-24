1a4b9d9295c0c54841f55d28463f2c6695951fe9
Remy Zulauff <remy.zulauff@gmail.com>
2023-11-24 19:40:08 +0100

    feat(git.py): add progress bar using rich library to display git clone progress in a more user-friendly way

--- odooghost/git.py
+++ odooghost/git.py
@@ -2,16 +2,82 @@ from pathlib import Path
 
 from git import RemoteProgress, Repo
 from loguru import logger
+from rich import console, progress
 
 
-class ProgressPrinter(RemoteProgress):
-    def update(self, op_code, cur_count, max_count=None, message=""):
-        print(
-            op_code,
-            cur_count,
-            max_count,
-            cur_count / (max_count or 100.0),
+class GitRemoteProgress(RemoteProgress):
+    OP_CODES = [
+        "BEGIN",
+        "CHECKING_OUT",
+        "COMPRESSING",
+        "COUNTING",
+        "END",
+        "FINDING_SOURCES",
+        "RECEIVING",
+        "RESOLVING",
+        "WRITING",
+    ]
+    OP_CODE_MAP = {getattr(RemoteProgress, _op_code): _op_code for _op_code in OP_CODES}
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.progressbar = progress.Progress(
+            progress.SpinnerColumn(),
+            # *progress.Progress.get_default_columns(),
+            progress.TextColumn("[progress.description]{task.description}"),
+            progress.BarColumn(),
+            progress.TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
+            "eta",
+            progress.TimeRemainingColumn(),
+            progress.TextColumn("{task.fields[message]}"),
+            console=console.Console(),
+            transient=False,
         )
+        self.progressbar.start()
+        self.active_task = None
+
+    def __del__(self) -> None:
+        # logger.info("Destroying bar...")
+        self.progressbar.stop()
+
+    @classmethod
+    def get_curr_op(cls, op_code: int) -> str:
+        """Get OP name from OP code."""
+        # Remove BEGIN- and END-flag and get op name
+        op_code_masked = op_code & cls.OP_MASK
+        return cls.OP_CODE_MAP.get(op_code_masked, "?").title()
+
+    def update(
+        self,
+        op_code: int,
+        cur_count: str | float,
+        max_count: str | float | None = None,
+        message: str | None = "",
+    ) -> None:
+        print("should pas")
+        # Start new bar on each BEGIN-flag
+        if op_code & self.BEGIN:
+            self.curr_op = self.get_curr_op(op_code)
+            # logger.info("Next: %s", self.curr_op)
+            self.active_task = self.progressbar.add_task(
+                description=self.curr_op,
+                total=max_count,
+                message=message,
+            )
+
+        self.progressbar.update(
+            task_id=self.active_task,
+            completed=cur_count,
+            message=message,
+        )
+
+        # End progress monitoring on each END-flag
+        if op_code & self.END:
+            # logger.info("Done: %s", self.curr_op)
+            self.progressbar.update(
+                task_id=self.active_task,
+                message=f"[bright_black]{message}",
+            )
 
 
 class Git:
@@ -23,7 +89,7 @@ class Git:
                 url=url,
                 to_path=path,
                 branch=branch,
-                progress=ProgressPrinter(),
+                progress=GitRemoteProgress(),
                 depth=depth,
             )
         except Exception as e:
